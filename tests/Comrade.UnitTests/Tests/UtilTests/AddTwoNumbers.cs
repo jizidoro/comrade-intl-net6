e59b699a277e892be0cf07f28c01d4bab4dcc664
jhonatan izidoro <jhonatanizidoro@gmail.com>
2022-10-27 20:12:38 -0300

    resolving new challenges

---
+++ tests/Comrade.UnitTests/Tests/UtilTests/AddTwoNumbers.cs
@@ -0,0 +1,112 @@
+﻿using Xunit;
+
+namespace Comrade.UnitTests.Tests.UtilTests;
+
+public class AddTwoNumbers
+{
+    [Fact]
+    public void AddTwoNumbersTests()
+    {
+        // [2,4,3]
+        var l1 = new ListNode(9, new ListNode(9, new ListNode(9)));
+
+        // [5,6,4]
+        var l2 = new ListNode(9, new ListNode(9, new ListNode(9)));
+
+        var tes1 = RecFun(l1, new List<int>());
+        var tes2 = RecFun(l2, new List<int>());
+
+        var oto3 = new List<int>();
+
+        var stack1 = new Stack<int>(tes1);
+        var stack2 = new Stack<int>(tes2);
+        var resto = 0;
+        while (stack1.Count > 0 || stack2.Count > 0)
+        {
+            var oto1 = stack1.TryPeek(out var result1);
+            var oto2 = stack2.TryPeek(out var result2);
+
+            if (resto > 0)
+            {
+                result1 = result1 + resto;
+                resto = 0;
+            }
+
+            if (oto1 || oto2 || result1 > 0)
+            {
+                var val = result1 + result2;
+
+                if (val >= 10)
+                {
+                    val = val -10;
+                    resto = 1;
+                }
+                
+                if(oto1)
+                {
+                    stack1.Pop();
+                }
+
+                if(oto2){
+                    stack2.Pop();
+                }
+                oto3.Add(val);
+            }
+        }
+
+        oto3.Reverse();
+        var result = new Stack<int>(oto3.ToArray());
+        var response = new ListNode(result.Pop());
+
+        while (result.Count > 0)
+        {
+            var asd = result.TryPeek(out var result1);
+            if (asd)
+            {
+                ResponseAssembly(response, result1);
+                result.Pop();
+            }
+        }
+
+        var tes22 = response;
+    }
+
+
+    public List<int> RecFun(ListNode node, List<int> tes)
+    {
+        if (node.next != null)
+        {
+            RecFun(node.next, tes);
+        }
+
+        tes.Add(node.val);
+        return tes;
+    }
+
+    public ListNode ResponseAssembly(ListNode node, int value)
+    {
+        if (node.next == null)
+        {
+            node.next = new ListNode(value);
+        }
+        else
+        {
+            ResponseAssembly(node.next, value);
+        }
+
+
+        return node;
+    }
+}
+
+public class ListNode
+{
+    public ListNode? next;
+    public int val;
+
+    public ListNode(int val = 0, ListNode? next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ tests/Comrade.UnitTests/Tests/UtilTests/FindMedianSortedArrays.cs
@@ -0,0 +1,128 @@
+﻿using Comrade.Application.Extensions;
+using IdentityModel.OidcClient;
+using Xunit;
+
+namespace Comrade.UnitTests.Tests.UtilTests;
+
+public class FindMedianSortedArrays
+{
+    [Fact]
+    public void FindMedianSortedArraysTests() {
+
+        //eu ia pegar os dos arrays, pegar o tamanho e somar
+        //eu ai achar a posicao da mediana
+        //iterar sobre os arrays ate tirar a posicao da mediana de dentro dos arrays
+
+        int[] nums1 = {0,0};
+        int[] nums2 = {0,0};
+
+        var stack1 = new Stack<int>(nums1);
+        var stack2 = new Stack<int>(nums2);
+
+        double mediana = (nums1.Length + nums2.Length)/2.0;
+
+        double oto = 0;
+
+        if (mediana % 1 == 0)
+        {
+            for (int i = 1; i < (int)mediana +1; i++)
+            {
+                var tes1 = stack1.TryPeek(out int result1);
+                var tes2 = stack2.TryPeek(out int result2);
+
+                if (i == (int)mediana)
+                {
+                    var qwe = new List<int>();
+                    for (int j = 0; j < 2; j++)
+                    {
+                        tes1 = stack1.TryPeek(out int resultF1);
+                        tes2 = stack2.TryPeek(out int resultF2);
+                        if (resultF1 > resultF2)
+                        {
+                            if (tes1)
+                            {
+                                qwe.Add(stack1.Pop());
+                            }
+                            else
+                            {
+                                qwe.Add(stack2.Pop());
+                            }
+                        }
+                        else
+                        {
+                            if (tes2)
+                            {
+                                qwe.Add(stack2.Pop());
+                            }
+                            else
+                            {
+                                qwe.Add(stack1.Pop());
+                            }
+                                
+                        }
+                    }
+
+                    oto = (qwe[0] + qwe[1])/2.0;
+
+                }
+                else
+                {
+                    if (result1 > result2)
+                    {
+                        if (tes1)
+                        {
+                            oto = stack1.Pop();
+                        }
+                    }
+                    else
+                    {
+                        if (tes2)
+                        {
+                            oto = stack2.Pop();
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < mediana; i++)
+            {
+                var tes1 = stack1.TryPeek(out int result1);
+                var tes2 = stack2.TryPeek(out int result2);
+
+                if (result1 > result2)
+                {
+                    if (tes1)
+                    {
+                        oto = stack1.Pop();
+                    }
+                    else
+                    {
+                        if (tes2)
+                        {
+                            oto = stack2.Pop();
+                        }
+                    }
+                }
+                else
+                {
+                    if (tes2)
+                    {
+                        oto = stack2.Pop();
+                    }
+                    else
+                    {
+                        if (tes1)
+                        {
+                            oto = stack1.Pop();
+                        }
+                    }
+                }
+            }
+        }
+
+        var oto2 = oto;
+    }
+}
+
